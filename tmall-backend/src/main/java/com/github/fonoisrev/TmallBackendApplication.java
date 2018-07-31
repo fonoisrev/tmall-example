179b555d1271eab56c848aa380624290e62bfbc1
fonoisrev <wuhangdeyouxiang@126.com>
2018-07-31 10:08:35 +0800

    添加项目基本结构

---
+++ tmall-backend/src/main/java/com/github/fonoisrev/TmallBackendApplication.java
@@ -0,0 +1,12 @@
+package com.github.fonoisrev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmallBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmallBackendApplication.class, args);
+	}
+}
